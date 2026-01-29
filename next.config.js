c08df205eb97952e39bc58683e4e5f5e905335d1
ajfahim <ajfahim52@gmail.com>
2026-01-29 17:18:45 +0600

    Update Next.js configuration to ignore TypeScript build errors and modify package-lock.json and yarn.lock to include peer dependencies for various packages

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ module.exports = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
     async rewrites() {
         return [
             {
